27963aa8ca0da16afdfd16b59f3db45e09f1d482
Mukesh200623 <142876342+Mukesh200623@users.noreply.github.com>
2023-08-31 10:29:17 +0530

    Update area.java

--- area.java
+++ area.java
@@ -3,6 +3,7 @@ public static void main(String args[]){
 double pi=3.14159;
 double r=10;
 double a=pi*r*r;
-System.out.println("the area of theb circle is:"+a);    
+System.out.println("the area of the
+                   circle is:"+a);    
 }   
 }
